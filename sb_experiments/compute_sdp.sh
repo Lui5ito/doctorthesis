dc1c2c89d9c3f814e5bbe730ab10a92ce3e8cf5d
Lui5ito <louis.allain@gmail.com>
2024-12-15 16:45:43 +0000

    pipeline for all sdp computation

---
+++ sb_experiments/compute_sdp.sh
@@ -0,0 +1,8 @@
+python generate_data.py --size 100 --seeds 123 124 125 126 127 321 322 323
+python generate_data.py --size 300 --seeds 987 986 985
+
+python train_gp.py
+
+python sdp_simultaneous.py
+python sdp_sim_calibration.py
+python sdp_sim_inference.py
