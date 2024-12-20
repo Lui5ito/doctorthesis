47db3bf94890d80675a32445e9a6ff9692a8bea5
Lui5ito <louis.allain@gmail.com>
2024-12-20 15:17:36 +0000

    compute sdp problem from train to predict

--- sb_experiments/e2e_sdp_mod.py
+++ sb_experiments/e2e_sdp_mod.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
     fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": S3_ENDPOINT_URL})
 
     # Lengthscales to compute 900
-    variance_lengthscale = 1e-9
+    list_lengthscales = [1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100]
     delta = 1e-3
     lambda2 = 1
     problem = "Liang"
@@ -131,56 +131,55 @@ if __name__ == "__main__":
     test_data_FILE_PATH_IN_S3 = test_data_FOLDER_PATH_IN_S3 + "data.npz"
     X_test, y_test = load_file(test_data_FILE_PATH_IN_S3, fs)
 
-    # Define the model
-    sdp_model = ub.UniversalFunctionAndBandsRegressor(
-        mean_kernel=kernels.Matern(length_scale=theta_m["posterior_lengthscale"], length_scale_bounds=(1e-5, 1e5), nu=2.5),
-        variance_kernel=kernels.Matern(length_scale=variance_lengthscale, length_scale_bounds=(1e-5, 1e5), nu=2.5),
-        lambda2=lambda2,
-        delta=delta,
-        s=theta_m["posterior_training_norm"],
-        problem=problem,
-        checkSDP=False,
-        verbose=True
-    )
-    print(f"Variance lengthscale {variance_lengthscale}.")
+    for variance_lengthscale in list_lengthscales:
+        # Define the model
+        sdp_model = ub.UniversalFunctionAndBandsRegressor(
+            mean_kernel=kernels.Matern(length_scale=theta_m["posterior_lengthscale"], length_scale_bounds=(1e-5, 1e5), nu=2.5),
+            variance_kernel=kernels.Matern(length_scale=variance_lengthscale, length_scale_bounds=(1e-5, 1e5), nu=2.5),
+            lambda2=lambda2,
+            delta=delta,
+            s=theta_m["posterior_training_norm"],
+            problem=problem,
+            checkSDP=False,
+            verbose=True
+        )
+        print(f"Variance lengthscale {variance_lengthscale}.")
 
-    # Train the model
-    sdp_model.train(X=X_train, y=y_train)
-    sdp_model.metrics["hsic_train_full"] = sdp_model.compute_HSIC(X=X_train, y=y_train, extra=False)
+        # Train the model
+        sdp_model.train(X=X_train, y=y_train)
+        sdp_model.metrics["hsic_train_full"] = sdp_model.compute_HSIC(X=X_train, y=y_train, extra=False)
 
-    # Calibrate the model
-    sdp_model.calibrate(X=X_cal, y=y_cal, alpha=alpha)
-    sdp_model.metrics["hsic_cal_full"] = sdp_model.compute_HSIC(X=X_cal, y=y_cal, extra=False)
+        # Calibrate the model
+        sdp_model.calibrate(X=X_cal, y=y_cal, alpha=alpha)
+        sdp_model.metrics["hsic_cal_full"] = sdp_model.compute_HSIC(X=X_cal, y=y_cal, extra=False)
 
-    # Predict
-    mean_prediction, variance_prediction = sdp_model.predict(X=X_test)
-    sdp_model.metrics["hsic_test_full"] = sdp_model.compute_HSIC(X=X_test, y=y_test, extra=False)
+        # Predict
+        # Predict
+        #X_test = np.concatenate((X_train, X_cal), axis=0)
+        #y_test = np.concatenate((y_train, y_cal), axis=0)
+        mean_prediction, variance_prediction = sdp_model.predict(X=X_test)
+        mean_width = np.mean(variance_prediction)
 
-    # Plot
-    figure = plot(
-        X=X_train,
-        y=y_train,
-        X_cal=X_cal,
-        y_cal=y_cal,
-        X_test=X_test,
-        y_test=y_test,
-        mean_estimation_test=mean_prediction,
-        bands_estimation_test=variance_prediction,
-        limits=(-3, 5),
-        title=f"SDP Model\nLengthscale is {variance_lengthscale}.",
-    )
+        # Plot
+        figure = plot(
+            X=X_train,
+            y=y_train,
+            X_cal=X_cal,
+            y_cal=y_cal,
+            X_test=X_test,
+            y_test=y_test,
+            mean_estimation_test=mean_prediction,
+            bands_estimation_test=variance_prediction,
+            limits=(-3, 5),
+            title=f"SDP Model\nLengthscale is {variance_lengthscale}.\nLambdahat is {sdp_model.lambdahat}.\nMean width is {mean_width}.\nHSIC train is {sdp_model.metrics["hsic_train_full"]}.\nHSIC cal is {sdp_model.metrics["hsic_cal_full"]}.",
+        )
 
-    FOLDER_PATH_OUT_S3 = f"luisito/these/sb_experiments/e2e_sdp/data_case_{case_number}/sample_shape_({sample_size},{sample_dim})/seed_{seed}/problem_{problem}/lambda2_{lambda2}/delta_{delta}/variance_lengthscale_{variance_lengthscale}/data_case_{cal_case_number}/sample_shape_({cal_sample_size},{cal_sample_dim})/seed_{cal_seed}/data_case_{test_case_number}/sample_shape_({test_sample_size},{test_sample_dim})/seed_{test_seed}/"
+        FOLDER_PATH_OUT_S3 = f"luisito/these/sb_experiments/e2e_sdp/data_case_{case_number}/sample_shape_({sample_size},{sample_dim})/seed_{seed}/problem_{problem}/lambda2_{lambda2}/delta_{delta}/variance_lengthscale_{variance_lengthscale}/data_case_{cal_case_number}/sample_shape_({cal_sample_size},{cal_sample_dim})/seed_{cal_seed}/data_case_{test_case_number}/sample_shape_({test_sample_size},{test_sample_dim})/seed_{test_seed}/"
 
 
-    plot_FILE_PATH_OUT_S3 = FOLDER_PATH_OUT_S3 + "figure.pdf"
-    with fs.open(plot_FILE_PATH_OUT_S3, mode="wb") as file_out:
-        figure.savefig(file_out, transparent=True, dpi='figure', format="pdf",
-        metadata=None,
-        bbox_inches=None, pad_inches=0.1, facecolor='auto', edgecolor='auto', backend=None)
-        plt.close()
-
-    # Save whole SDP model
-    model_FILE_PATH_OUT_S3 = FOLDER_PATH_OUT_S3 + "sdp_model.pkl"
-    with fs.open(model_FILE_PATH_OUT_S3, mode="wb") as file_out:
-        pickle.dump(sdp_model, file_out)
+        plot_FILE_PATH_OUT_S3 = FOLDER_PATH_OUT_S3 + "figure_bands_test.pdf"
+        with fs.open(plot_FILE_PATH_OUT_S3, mode="wb") as file_out:
+            figure.savefig(file_out, transparent=True, dpi='figure', format="pdf",
+            metadata=None,
+            bbox_inches=None, pad_inches=0.1, facecolor='auto', edgecolor='auto', backend=None)
+            plt.close()
