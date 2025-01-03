551b9d45c926109a5fe081634fced42a0bf70700
Lui5ito <louis.allain@gmail.com>
2025-01-03 14:26:45 +0000

    full routine in a single sh file

--- sb_experiments/fullOpt.sh
+++ sb_experiments/fullOpt.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-#python data/generate_data.py --cases 8 --sizes 100 200 --seeds 123 321
-#python gp/train_gp.py --cases 8 --sizes 100 200 --seeds 123
-#python data/generate_data.py --cases 8 --sizes 300 1000 2000 --seeds 987
+CASE=5
+SIZE=20
+DIM=2
+SEED=123
+CAL_SEED=321
+TEST_SEED=987
+TEST_SIZE=300
 
-python optimise_lengthscale/optimise_Kfolds.py --case_number 8 --size 100 --dim 1 --seed 123 --num_folds 10 --l_bound 1e-8 --u_bound 10 --max_eval 100 --n_restarts 10
-python optimise_lengthscale/optimise_Kfolds.py --case_number 8 --size 200 --dim 1 --seed 123 --num_folds 10 --l_bound 1e-8 --u_bound 10 --max_eval 100 --n_restarts 10
+python data/generate_data.py --cases $CASE --sizes $SIZE --dims $DIM --seeds $SEED $CAL_SEED
+python gp/train_gp.py --cases $CASE --sizes $SIZE --dims $DIM --seeds $SEED
+python data/generate_data.py --cases $CASE --sizes $TEST_SIZE --dims $DIM --seeds $TEST_SEED
+python optimise_lengthscale/optimise_Kfolds.py --case_number $CASE --size $SIZE --dim $DIM --seed $SEED --num_folds 10 --l_bound 1e-8 --u_bound 10 --max_eval 100 --n_restarts 2
+python optimise_lengthscale/afterTrain.py --train $CASE $SIZE $DIM $SEED --calibration $CASE $SIZE $DIM $CAL_SEED --test $CASE $TEST_SIZE $DIM $TEST_SEED
