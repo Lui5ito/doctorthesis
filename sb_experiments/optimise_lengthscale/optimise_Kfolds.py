b8014268b071607a058fd3a6f2e6fde01f273698
Lui5ito <louis.allain@gmail.com>
2025-01-03 14:27:46 +0000

    ruff refactor

--- sb_experiments/optimise_lengthscale/optimise_Kfolds.py
+++ sb_experiments/optimise_lengthscale/optimise_Kfolds.py
@@ -14,8 +14,8 @@ import time
 
 def load_file(file_path, fs):
     """Load a file based on its extension."""
-    
-    file_extension = file_path.split('.')[-1]  # Get the file extension
+
+    file_extension = file_path.split(".")[-1]  # Get the file extension
 
     if file_extension == "npz":
         # Load .npz file
@@ -40,21 +40,25 @@ def load_file(file_path, fs):
     else:
         raise ValueError(f"Unsupported file format: {file_extension}")
 
-def restart_optimise(num_restarts, settings):
 
+def restart_optimise(num_restarts, settings):
     # Define bounds for the parameters.
-    lower_bounds = np.array([settings["lower_bound"]])
-    upper_bounds = np.array([settings["upper_bound"]])
+    lower_bounds = np.array(settings["lower_bound"])
+    upper_bounds = np.array(settings["upper_bound"])
 
     last_theta = []
     last_hsic = []
 
     for restart in range(num_restarts):
         log = {"restart": restart, "try": 0}
-        
-        objective_function_one_param = partial(objective_function, settings=settings, log=log)
-        
-        initial_theta_v = np.random.uniform(lower_bounds.flatten(), upper_bounds.flatten(), settings["sample_dim"])
+
+        objective_function_one_param = partial(
+            objective_function, settings=settings, log=log
+        )
+
+        initial_theta_v = np.random.uniform(
+            lower_bounds.flatten(), upper_bounds.flatten(), settings["sample_dim"]
+        )
         opt = nlopt.opt(nlopt.LN_BOBYQA, len(initial_theta_v))
         opt.set_lower_bounds(lower_bounds.flatten())
         opt.set_upper_bounds(upper_bounds.flatten())
@@ -70,8 +74,9 @@ def restart_optimise(num_restarts, settings):
 
         last_theta.append(theta_v_opt)
         last_hsic.append(max_hsic)
-    
+
     return last_theta, last_hsic
+
 
 def objective_function(theta_v, grad, settings, log):
     # Unpack settings
@@ -88,7 +93,7 @@ def objective_function(theta_v, grad, settings, log):
     # Unpack log
     restart_num = log["restart"]
     try_num = log["try"]
-    
+
     # Get train data
     data_FOLDER_PATH_IN_S3 = f"luisito/these/sb_experiments/data/case_{case_number}/sample_shape_({sample_size},{sample_dim})/seed_{seed}/"
     data_FILE_PATH_IN_S3 = data_FOLDER_PATH_IN_S3 + "data.npz"
@@ -107,8 +112,14 @@ def objective_function(theta_v, grad, settings, log):
     for k, (train_index, test_index) in enumerate(kf.split(X_train)):
         # Define the model
         sdp_model = ub.UniversalFunctionAndBandsRegressor(
-            mean_kernel=kernels.Matern(length_scale=theta_m["posterior_lengthscale"], length_scale_bounds=(1e-5, 1e5), nu=2.5),
-            variance_kernel=kernels.Matern(length_scale=theta_v, length_scale_bounds=(1e-5, 1e5), nu=2.5),
+            mean_kernel=kernels.Matern(
+                length_scale=theta_m["posterior_lengthscale"],
+                length_scale_bounds=(1e-5, 1e5),
+                nu=2.5,
+            ),
+            variance_kernel=kernels.Matern(
+                length_scale=theta_v, length_scale_bounds=(1e-5, 1e5), nu=2.5
+            ),
             lambda2=lambda2,
             delta=delta,
             s=theta_m["posterior_training_norm"],
@@ -118,14 +129,16 @@ def objective_function(theta_v, grad, settings, log):
         )
         # Train the model
         sdp_model.train(X=X_train[train_index], y=y_train[train_index])
-        hsic_value, abs_errors, half_width = sdp_model.compute_HSIC(X=X_train[test_index], y=y_train[test_index], extra=True)
+        hsic_value, abs_errors, half_width = sdp_model.compute_HSIC(
+            X=X_train[test_index], y=y_train[test_index], extra=True
+        )
         folded_AE.append(abs_errors)
         folded_IW.append(half_width)
 
     folded_AE = np.vstack(folded_AE)
     folded_IW = np.vstack(folded_IW)
     kfold_hsic = Energy_HSIC(abs_error=folded_AE, width=folded_IW).compute()
-    
+
     log["try"] += 1
 
     return kfold_hsic
@@ -141,7 +154,7 @@ def save_best_model(theta_v, settings, bobyqa_time):
     sample_dim = settings["sample_dim"]
     seed = settings["seed"]
     fs = settings["fs"]
-    
+
     # Get train data
     data_FOLDER_PATH_IN_S3 = f"luisito/these/sb_experiments/data/case_{case_number}/sample_shape_({sample_size},{sample_dim})/seed_{seed}/"
     data_FILE_PATH_IN_S3 = data_FOLDER_PATH_IN_S3 + "data.npz"
@@ -153,15 +166,21 @@ def save_best_model(theta_v, settings, bobyqa_time):
     theta_m = load_file(gp_FILE_PATH_IN_S3, fs)
 
     sdp_model = ub.UniversalFunctionAndBandsRegressor(
-            mean_kernel=kernels.Matern(length_scale=theta_m["posterior_lengthscale"], length_scale_bounds=(1e-5, 1e5), nu=2.5),
-            variance_kernel=kernels.Matern(length_scale=theta_v, length_scale_bounds=(1e-5, 1e5), nu=2.5),
-            lambda2=lambda2,
-            delta=delta,
-            s=theta_m["posterior_training_norm"],
-            problem=problem,
-            checkSDP=False,
-            verbose=True,
-        )
+        mean_kernel=kernels.Matern(
+            length_scale=theta_m["posterior_lengthscale"],
+            length_scale_bounds=(1e-5, 1e5),
+            nu=2.5,
+        ),
+        variance_kernel=kernels.Matern(
+            length_scale=theta_v, length_scale_bounds=(1e-5, 1e5), nu=2.5
+        ),
+        lambda2=lambda2,
+        delta=delta,
+        s=theta_m["posterior_training_norm"],
+        problem=problem,
+        checkSDP=False,
+        verbose=True,
+    )
     sdp_model.train(X=X_train, y=y_train)
     sdp_model.metrics["bobyqa_time"] = bobyqa_time
 
@@ -171,21 +190,21 @@ def save_best_model(theta_v, settings, bobyqa_time):
         pickle.dump(sdp_model, file_out)
 
 
-
-
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description='Argparse for BOBYQA optimisation of the lenthscale using k-folds.')
-    parser.add_argument('--case_number', type=int)
-    parser.add_argument('--size', type=int)
-    parser.add_argument('--dim', type=int)
-    parser.add_argument('--seed', type=int)
-    parser.add_argument('--num_folds', type=int)
-    parser.add_argument('--l_bound', nargs='+', type=float)
-    parser.add_argument('--u_bound', nargs='+', type=float)
-    parser.add_argument('--max_eval', type=int)
-    parser.add_argument('--n_restarts', type=int)
+    parser = argparse.ArgumentParser(
+        description="Argparse for BOBYQA optimisation of the lenthscale using k-folds."
+    )
+    parser.add_argument("--case_number", type=int)
+    parser.add_argument("--size", type=int)
+    parser.add_argument("--dim", type=int)
+    parser.add_argument("--seed", type=int)
+    parser.add_argument("--num_folds", type=int)
+    parser.add_argument("--l_bound", type=float)
+    parser.add_argument("--u_bound", type=float)
+    parser.add_argument("--max_eval", type=int)
+    parser.add_argument("--n_restarts", type=int)
     args = parser.parse_args()
 
     print(f"Starting optimisation for seed {args.seed}")
@@ -204,18 +223,22 @@ if __name__ == "__main__":
         "seed": args.seed,
         "num_folds": args.num_folds,
         "fs": s3fs.S3FileSystem(client_kwargs={"endpoint_url": S3_ENDPOINT_URL}),
-        "lower_bound": args.l_bound,
-        "upper_bound": args.u_bound,
+        "lower_bound": [args.l_bound for i in range(args.dim)],
+        "upper_bound": [args.u_bound for i in range(args.dim)],
         "max_eval": args.max_eval,
     }
 
     start = time.time()
-    best_theta, best_hsic = restart_optimise(num_restarts=args.n_restarts, settings=settings)
+    best_theta, best_hsic = restart_optimise(
+        num_restarts=args.n_restarts, settings=settings
+    )
     end = time.time()
-    elapsed = (end - start)
+    elapsed = end - start
 
     index_hsic = best_hsic.index(max(best_hsic))
     print(f"Best lengthscale found is {best_theta[index_hsic]}.")
 
     # Save best model
-    save_best_model(theta_v=best_theta[index_hsic], settings=settings, bobyqa_time=elapsed)
+    save_best_model(
+        theta_v=best_theta[index_hsic], settings=settings, bobyqa_time=elapsed
+    )
